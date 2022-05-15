6beb3179faf639646eba6205c6cdc154bde799a8
MJerich <MatthewJerich@gmail.com>
2022-05-15 15:51:00 -0700

    fixed content bug

--- models/Post.js
+++ models/Post.js
@@ -37,7 +37,7 @@ Post.init(
             allowNull: false
         },
         content: {
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false,
         },
         user_id: {
